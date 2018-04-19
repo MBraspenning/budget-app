85a76da1dd244ce323f5a067dc6a4ff9dd23005f
Matthijs Braspenning <matthijsbraspenning@gmail.com>
2018-04-19 20:34:57 +0200

    fixes tabs in navigation.php

--- app/views/partials/navigation.php
+++ app/views/partials/navigation.php
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-        <a class="nav-item nav-link" href="<?php echo URLROOT; ?>">Home</a>
-        <a class="nav-item nav-link" href="<?php echo URLROOT; ?>/archive">Archive</a>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+            <a class="nav-item nav-link" href="<?php echo URLROOT; ?>">Home</a>
+            <a class="nav-item nav-link" href="<?php echo URLROOT; ?>/archive">Archive</a>
+        </div>
     </div>
-  </div>
 </nav>
